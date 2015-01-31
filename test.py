1e3f4d7744af3ee7ea4d47ffac43e02548e068e8
Eric Chaves <erchaves@gmail.com>
2015-01-31 15:18:05 -0500

    updating report file
    walks the directories, makes a dummy report file

--- test.py
+++ test.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python 
 
-import os
 import re
+import os
 
 fileMustStartWith = 'disc'
-fileDirectoryName = 'sample_data'
+dataDirectoryName = 'sample_data'
 reportFileName = 'report.txt'
 
-files = [filename for filename in os.listdir(fileDirectoryName) if filename.startswith(fileMustStartWith)]
+def getFilePathsByLocation(directoryName):
+    validFilesByLocation = {}
 
-for fileName in files:
-    
-    readFile = open(fileDirectoryName + '/' + fileName, 'r')
+    for subdir, dirs, files in os.walk(directoryName):
+        validFiles = [filename for filename in files if filename.startswith(fileMustStartWith)]
+        folderName = subdir.split('/')[-1]
+        locationName = folderName
+
+        for validfile in validFiles:
+            filePath = os.path.join(subdir, validfile)
+            # todo: use a differnt key because locationname might not be a valid key
+            locationFiles = validFilesByLocation.setdefault(locationName,[])
+            locationFiles.append(filePath)
+
+    return validFilesByLocation
+
+def makeReport (filePathsByLocation, reportFileName):
+    # clear the file
     reportFile = open(reportFileName, 'w')
 
-    # print readFile.name
-    formattedLines = []
+    for location in filePathsByLocation:
+        filePaths = filePathsByLocation[location]
 
-    for line in readFile:
-        if re.match('(.*)', line):
-            formatedLine = re.sub('\s+', ',', line)
-            formattedLines.append(formatedLine)
+        for filePath in filePaths:
+            # open the file to append new lines
+            reportFile = open(reportFileName, 'a')
+        
+            # todo: do something with this
+            readFile = open(filePath, 'r')
+            readFile.close()
 
-    with reportFile as out:
-        out.writelines(formattedLines)
+            with reportFile as out:
+                out.writelines('test')
+                out.writelines('\n')
 
-    readFile.close()
     reportFile.close()
+
+filePathsByLocation = getFilePathsByLocation(dataDirectoryName)
+makeReport(filePathsByLocation, reportFileName)
