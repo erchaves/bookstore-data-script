bf979c749d0514c5736ed7e23dc4fe2752863231
Eric Chaves <erchaves@gmail.com>
2015-02-05 16:48:19 -0500

    updates
    Worked from a copy outside of git for a bit there, so no detailed commits.
    
    renamed sample_data to process_home, updates to parsing data, creating a csv instead of txt

--- makeReport.py
+++ makeReport.py
@@ -1,57 +1,96 @@
 #!/usr/bin/env python 
-
 import re
 import os
 
-fileMustStartWith = 'disc'
-dataDirectoryName = 'sample_data'
-reportFileName = 'report.txt'
-# validate if needed
-dataLineValidator = '.*'
+# variable settings
+shouldFilterZeroValues = True
+directoryPath = 'process_home'
+reportFilePath = 'report.csv'
+reportFields = ['date','locationName','discountType','quantity','amount']
+reportHeaders = ['Date,Location,Discount Type,Quantity,Amount']
+dateRegex = '\d\d_\d\d_\d\d'
+dateSearchRegex = '^.*('+dateRegex+').*$'
+# only process files that will be in the expected format
+fileNameValidator = '^disc.*'+dateRegex+'$'
+# add validation regex for data rows if desired.
+dataRowValidator = None
+# The specfic character ranges occupied by each field in a data row
+dataFieldCharRanges = {
+    'discountType': [0,20],
+    'quantity': [21,27],
+    'amount': [28,37],
+}
 
-def getFilePathsByLocation(directoryName):
-    validFilesByLocation = {}
+def getfileMetaDataArr(directoryName):
+    fileMetaDataArr = []
 
     for subdir, dirs, files in os.walk(directoryName):
-        validFiles = [filename for filename in files if filename.startswith(fileMustStartWith)]
+        validFiles = [filename for filename in files if re.match(fileNameValidator, filename)]
         folderName = subdir.split('/')[-1]
         locationName = folderName
-
+        
         for validfile in validFiles:
             filePath = os.path.join(subdir, validfile)
-            # todo: use a differnt key because locationname might not be a valid key
-            locationFiles = validFilesByLocation.setdefault(locationName,[])
-            locationFiles.append(filePath)
+            dateSearch = re.search(dateSearchRegex, validfile)
+            dateMatch = dateSearch.group(1)
+            fileMetaDataArr.append({
+                'date': dateMatch.replace('_','/'),
+                'filePath': filePath,
+                'locationName': locationName,
+            })
 
-    return validFilesByLocation
+    return fileMetaDataArr
 
-def formatDataLine (line):
-    # todo update to correct formatting
-    if not re.match(dataLineValidator, line):
+def formatDataRow (line, fileMetaData):
+    if dataRowValidator and not re.match(dataRowValidator, line):
         return ''
 
-    formatedLine = re.sub('\s+', ',', line)
-    return formatedLine + '\n'
+    fileRowData = parseDataRow(line)
+    dataDict = dict(fileRowData.items() + fileMetaData.items())
 
-def makeReport (filePathsByLocation, reportFileName):
-    # clear the file
-    reportFile = open(reportFileName, 'w').close()
+    if shouldFilterZeroValues:
+        hasZeros = not (float(dataDict['quantity']) and float(dataDict['amount']))
+        if hasZeros:
+            return ''
 
-    # todo: break into smaller functions
-    for location in filePathsByLocation:
-        filePaths = filePathsByLocation[location]
+    fields = [dataDict[field] for field in reportFields]
+    formattedLine = ','.join(map(str, fields))
+    return formattedLine
 
-        for filePath in filePaths:
-            # open the file to append new lines
-            readFile = open(filePath, 'r')
-            for line in readFile:
-                reportFile = open(reportFileName, 'a')
-                with reportFile as out:
-                    formattedLine = formatDataLine(line)
-                    out.writelines(formattedLine)
-                reportFile.close()
+# parses a line of raw data in a specific format where each field holds a set number of chars
+# Example of a row 'A Field..........10% 000014  000003421'
+def parseDataRow (line):
+    dataFields = {}
+    for key in dataFieldCharRanges:
+        range = dataFieldCharRanges[key]
+        field = line[range[0]:range[1]]
+        dataFields[key] = field
+    return dataFields
 
-            readFile.close()
+def writeLinesToReport (lines):
+    reportFile = open(reportFilePath, 'a')
+    with reportFile as out:
+        for line in lines:
+            if line:
+                out.writelines(line + '\n')
+    reportFile.close()
 
-filePathsByLocation = getFilePathsByLocation(dataDirectoryName)
-makeReport(filePathsByLocation, reportFileName)
+def makeCsvReport (fileMetaDataArr):
+    # first clear the file
+    reportFile = open(reportFilePath, 'w').close()
+
+    # write a header line
+    headerLine = ','.join(map(str, reportHeaders))
+    writeLinesToReport([headerLine])
+
+    # write the rest of the lines
+    for fileMetaData in fileMetaDataArr:
+        filePath = fileMetaData['filePath']
+        readFile = open(filePath, 'r')
+        formattedLines = [formatDataRow(line, fileMetaData) for line in readFile]
+        writeLinesToReport(formattedLines)
+        readFile.close()
+
+if __name__ == "__main__":
+    fileMetaDataArr = getfileMetaDataArr(directoryPath)
+    makeCsvReport(fileMetaDataArr)
